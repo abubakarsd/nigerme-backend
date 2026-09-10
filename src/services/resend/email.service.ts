af34cedd637cedc3d091f810eba02216d04cf660
Abubakar Haruna Abdulmalik <42899173+abubakarsd@users.noreply.github.com>
2026-09-10 16:55:17 +0100

    refactor: update BRAND_LOGO_URL to use a local relative path instead of an S3 bucket URL

--- src/services/resend/email.service.ts
+++ src/services/resend/email.service.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import { env } from "../../config/env.js";
 
-export const BRAND_LOGO_URL = "https://busmailer-172147427546-us-east-1-an.s3.us-east-1.amazonaws.com/favicon.png";
+export const BRAND_LOGO_URL = "/images/logo/favicon.png";
 export const ADVERT_BANNER_URL = "https://busmailer-172147427546-us-east-1-an.s3.us-east-1.amazonaws.com/advert-banner.png";
 export const FOOTER_BANNER_URL = "https://busmailer-172147427546-us-east-1-an.s3.us-east-1.amazonaws.com/footer-banner.png";
 
